6eb8b4a80dc477c4f872715c5081f043fc0d53b9
Chima-OL <ctw21.chima.olugbue@corketb.ie>
2025-09-08 13:57:42 +0100

    Add files via upload

---
+++ worksheet 1 exersices.py
@@ -0,0 +1,15 @@
+# date: 2/9/25
+# Author: chima olugbue
+
+print ("Name: Chima OLugbue")
+print ("Address: Carrigtwohill")
+print ("###### ###### ######")
+print ("    ## ##     ##")
+print ("###### ###### ######")
+print ("    ## ##  ##     ##")
+print ("###### ###### ######")
+print (" likes")
+print ()
+print ("1. Matching brackets")
+print ("2. double qoute symbols")
+
