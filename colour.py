d663877b6ed9a63cd9c3a1b9ec607409e7179f22
Chima-OL <ctw21.chima.olugbue@corketb.ie>
2025-09-16 14:18:31 +0100

    Add files via upload

---
+++ colour.py
@@ -0,0 +1,16 @@
+from turtle import *
+
+def triangle():
+    fillcolor("pink")
+    begin_fill()
+    pencolor("black")
+    forward(45)
+    right(60)
+    forward(100)
+    right(75)
+    fd(100)
+    right(35)
+    
+
+triangle()
+    
