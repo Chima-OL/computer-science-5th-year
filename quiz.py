efd6b8edd86ad1593813c38f9aa465aa6e60a814
Chima-OL <ctw21.chima.olugbue@corketb.ie>
2025-09-16 08:05:23 +0100

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,22 @@
+def quiz():
+    print("do you like apples")
+    print()
+    print("1. Absolutely")
+    print("2. Yes")
+    print("3. No")
+    print(" Absolutely not")
+    print()
+    print(" Pick an option (1,2,3 or 4")
+    answer= input()
+    if answer =="1":
+        print("You must really love apples")
+    elif answer =="2":
+         print("You like apples")
+    elif answer =="3":
+         print("Thats ok we cand find another fruit for you")
+    else:
+        print("DAMM ok u dont lime apples maybe oranges?")
+        
+
+quiz()
+    
