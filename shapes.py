83e086ab2d00831e9e1ff76d4d483ec40ba40edf
Chima-OL <ctw21.chima.olugbue@corketb.ie>
2025-09-18 07:23:18 +0100

    Add files via upload

---
+++ shapes.py
@@ -0,0 +1,41 @@
+from turtle import *
+
+def square():
+    fillcolor("blue")
+    begin_fill()
+    pencolor("black")
+    fd(100)
+    right(90)
+    fd(100)
+    right(90)
+    fd(100)
+    right(90)
+    fd(100)
+    end_fill()
+    
+square()
+
+
+def triangle():
+    fillcolor("yellow")
+    begin_fill()
+    pencolor("black")
+    fd(100)
+    left(120)
+    fd(100)
+    left(120)
+    fd(100)
+    left(120)
+    end_fill()
+    
+triangle()
+
+import turtle
+
+radius = 40
+turtle.circle(radius)
+input()
+
+
+
+    
